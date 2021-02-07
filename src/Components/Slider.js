173c289fc0de7671583c1cbd2847bc351b29028d
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-07 02:30:46 +0100

    slider - step1

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
-import Card from './Card'
 
+const Slider = ({ sliderData, SlideComponent }) => {
 
-const Slider = ({sliderData, SlideComponent}) => {
-const [current, setCurrent]= useState(0)
+    const [current, setCurrent] = useState(0)
+
+    const nextSlide = () => {
+        setCurrent(current < sliderData.length - 1 ? current + 1 : 0)
+        console.log(current)
+    }
+    const prevSlide = () => {
+        setCurrent(current > 0 ? current - 1 : sliderData.length - 1)
+    }
+
     return (
         <div>
-            <div className='slider__container'>
-                <div className='slider__btn' >move</div>
-                <div className='slider'>
-                    {sliderData.map((item, index) => <div className={index===current ? 'slide slide--actice' : 'slide'}>
-                        <SlideComponent data={item} key={index}/>
-                        </div>)}
-                </div>
-                <div className='slider__btn'>move</div>
+            <div className='slider__btn slider__btn--left' onClick={nextSlide}>move next</div>
+                <div className='slider__btn' onClick={prevSlide}>move prev</div>
+            <div className='slider'>
+                {sliderData.map((item, index) => <div key={index} className={index === current ? 'slide slide--active' : 'slide'}>
+                    <SlideComponent data={item} key={index} />
+                </div>)}
+            </div>
+            <div className='slider__counter'>
+                {sliderData.map((item, index) => <div key={index} className={index === current ? 'dot dot--active' : 'dot'}></div>)}
             </div>
         </div>
     )
