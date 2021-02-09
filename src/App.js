963c9310219a19a7dbdc3cf55cc2de8df4fac762
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-09 12:03:55 +0100

    router app and navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router,  Switch,  Route}  from 'react-router-dom'
+
 import AboutSlider from './Pages/FrontPage.js/AboutSlider';
 import FrontPage from './Pages/FrontPage.js/FrontPage';
 import SignIn from './Pages/FrontPage.js/SignIn'
 
 function App() {
   return (
-    <div>
-      <FrontPage />
-      <AboutSlider />
-      <SignIn />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={FrontPage}/>
+        <Route exact path='/about' component={AboutSlider}/>
+        <Route exact path='/signin' component={SignIn}/>
+      </Switch>
+    </Router>
   );
 }
 
