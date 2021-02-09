88d6094ed10624a0b3cd1ce7077af69818a270a0
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-09 21:11:08 +0100

    onClick added

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -6,13 +6,15 @@ export class Button extends Component {
         type: PropTypes.oneOf(['button', 'submit']),
         size: PropTypes.oneOf(['full', 'small', 'round']),
         variant: PropTypes.oneOf(['primary', 'secondary', 'primary-neutral', 'secondary-neutral']),
+        onClick: PropTypes.func,
     }
 
     render() {
         return (
             <button
             type={`${this.props.size || 'button'} `}
-            className={`button button--${this.props.size} button--${this.props.variant}`}>
+            className={`button button--${this.props.size} button--${this.props.variant}`}
+            onClick={this.props.onClick}>
                 {this.props.children}
             </button>
         )
