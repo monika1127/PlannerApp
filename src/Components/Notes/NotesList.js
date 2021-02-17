28a6c2391776a607b4fc552fa3f4e78e6005b558
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-17 12:45:42 +0100

    Note components and database draft

---
+++ src/Components/Notes/NotesList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NotesList = () => {
+  return (
+    <div className="notes-list">
+      <div className="notes-list__header">
+        <div className="notes-list__title">My Notes</div>
+      </div>
+      <div className="notes-list__items-container">
+        <div>coś</div>
+        <div>coś</div>
+        <div>coś</div>
+      </div>
+      <div className="notes-list__add-new">
+        <div>input file</div>
+      </div>
+    </div>
+  );
+};
+
+export default NotesList;
