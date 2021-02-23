f55b7a1e607ec19dfcdbe204980048abb1d9c10d
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-23 22:37:12 +0100

    styling improvement - up to md

--- src/Components/DashboardHome.js
+++ src/Components/DashboardHome.js
@@ -4,22 +4,18 @@ import mindMapPicture from '../assets/pictures/mind-map.svg';
 const DashboardHome = () => {
   return (
     <div className="dasboard-home">
-      <div className="dasboard-home-picture">
+      <div className="dasboard-home__picture">
         <img src={mindMapPicture} alt="MindMap" />
       </div>
-      <div className="dasboard-home-description">
-        <div>Start planning</div>
-        <div>
+      <div className="dasboard-home__description">
+        <div className="dasboard-home__title">Start planning</div>
+        <div className="dasboard-home__text">
           Nulla mollis suscipit nibh, et auctor nunc finibus eleifend. Morbi
           eget nunc eleifend, mollis nunc vitae, consectetur magna. Nulla
           posuere massa a eros interdum semper. Morbi vitae sapien ex. Fusce
           finibus elementum purus sed vestibulum. Vestibulum interdum ligula a
           tincidunt ullamcorper. Ut ac turpis ullamcorper, sodales velit in,
-          gravida arcu. Fusce sed neque non mi vestibulum dapibus at a odio.
-          Nulla sodales, orci posuere gravida pulvinar, purus orci mollis ex,
-          non condimentum lorem dui et metus. Etiam sollicitudin justo et mi
-          rhoncus interdum. Proin facilisis dapibus justo, vitae vehicula mi
-          gravida eget.{' '}
+          gravida arcu.
         </div>
       </div>
     </div>
