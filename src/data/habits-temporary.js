6381e989df21acc6aa7faad9e4206c453ad1ac3f
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-25 17:46:19 +0100

    calendar - custom date picker

---
+++ src/data/habits-temporary.js
@@ -0,0 +1,23 @@
+export const habitsArr = [
+  {
+    name: 'zakupy',
+    dateCreated: '2021-02-21',
+    weeklyFrequency: [3, 4, 5],
+    color: 'blue',
+    history: [{ '2021-02-24': true }],
+  },
+  {
+    name: 'stretching',
+    dateCreated: '2021-02-21',
+    weeklyFrequency: [1, 3, 4],
+    color: 'blue',
+    history: [{ '2021-02-21': false }, { '2021-02-22': false }],
+  },
+  {
+    name: 'twarzing',
+    dateCreated: '2021-02-23',
+    weeklyFrequency: [3, 5],
+    color: 'blue',
+    history: [{ '2021-02-24': true }],
+  },
+];
