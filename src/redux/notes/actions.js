a5a48fa12fa88ac3066f6f72e2aef88a9c8ef339
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-28 15:59:07 +0100

    init migration to node server

--- src/redux/notes/actions.js
+++ src/redux/notes/actions.js
@@ -9,8 +9,6 @@ import {
   SORT_NOTE_ITEMS,
   SET_LOADING,
 } from './types';
-import firebase from '../../firebaseConfig';
-import { doc } from 'prettier';
 
 export const setLoading = () => {
   return {
@@ -20,135 +18,131 @@ export const setLoading = () => {
 
 export const getNotesCategories = () => async (dispatch) => {
   dispatch(setLoading());
-  try {
-    const response = await firebase
-      .firestore()
-      .collection('notesCategories')
-      .get();
+  // try {
+  //   const response = await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .get();
 
-    const categories = await response.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
+  //   const categories = await response.docs.map((doc) => ({
+  //     ...doc.data(),
+  //     id: doc.id,
+  //   }));
 
-    dispatch({
-      type: GET_NOTES_LIST,
-      payload: categories,
-    });
-  } catch (err) {}
+  //   dispatch({
+  //     type: GET_NOTES_LIST,
+  //     payload: categories,
+  //   });
+  // } catch (err) {}
 };
 
 export const getNoteItems = (noteID) => async (dispatch) => {
   dispatch(setLoading());
-  try {
-    const response = await firebase
-      .firestore()
-      .collection('notesCategories')
-      .doc(noteID)
-      .collection('todos')
-      .get();
+  // try {
+  //   const response = await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .doc(noteID)
+  //     .collection('todos')
+  //     .get();
 
-    const noteItems = await response.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
+  //   const noteItems = await response.docs.map((doc) => ({
+  //     ...doc.data(),
+  //     id: doc.id,
+  //   }));
 
-    dispatch({
-      type: GET_NOTE_ITEMS,
-      payload: noteItems,
-    });
-  } catch (err) {}
+  //   dispatch({
+  //     type: GET_NOTE_ITEMS,
+  //     payload: noteItems,
+  //   });
+  // } catch (err) {}
 };
 
 export const changeItemStatus = (noteID, todoItemID, values) => async (
   dispatch,
 ) => {
-  try {
-    await firebase
-      .firestore()
-      .collection('notesCategories')
-      .doc(noteID)
-      .collection('todos')
-      .doc(todoItemID)
-      .update(values);
-
-    const updatedItem = { ...values, id: todoItemID };
-    dispatch({
-      type: UPDATE_STATUS,
-      payload: updatedItem,
-    });
-  } catch (err) {
-    console.log({ err });
-  }
+  // try {
+  //   await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .doc(noteID)
+  //     .collection('todos')
+  //     .doc(todoItemID)
+  //     .update(values);
+  //   const updatedItem = { ...values, id: todoItemID };
+  //   dispatch({
+  //     type: UPDATE_STATUS,
+  //     payload: updatedItem,
+  //   });
+  // } catch (err) {
+  //   console.log({ err });
+  // }
 };
 
 export const deleteItem = (noteID, todoItemID) => async (dispatch) => {
   setLoading();
-  try {
-    await firebase
-      .firestore()
-      .collection('notesCategories')
-      .doc(noteID)
-      .collection('todos')
-      .doc(todoItemID)
-      .delete();
-    dispatch({
-      type: DELETE_NOTE_ITEM,
-      payload: todoItemID,
-    });
-  } catch (err) {}
+  // try {
+  //   await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .doc(noteID)
+  //     .collection('todos')
+  //     .doc(todoItemID)
+  //     .delete();
+  //   dispatch({
+  //     type: DELETE_NOTE_ITEM,
+  //     payload: todoItemID,
+  //   });
+  // } catch (err) {}
 };
 
 export const addNoteItem = (noteID, values, callback) => async (dispatch) => {
-  try {
-    const response = await firebase
-      .firestore()
-      .collection('notesCategories')
-      .doc(noteID)
-      .collection('todos')
-      .add(values);
-    const data = await (await response.get()).data();
-
-    const newItem = { ...data, id: response.id };
-    dispatch(
-      {
-        type: ADD_NOTE_ITEM,
-        payload: newItem,
-      },
-      callback(),
-    );
-  } catch (err) {}
+  // try {
+  //   const response = await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .doc(noteID)
+  //     .collection('todos')
+  //     .add(values);
+  //   const data = await (await response.get()).data();
+  //   const newItem = { ...data, id: response.id };
+  //   dispatch(
+  //     {
+  //       type: ADD_NOTE_ITEM,
+  //       payload: newItem,
+  //     },
+  //     callback(),
+  //   );
+  // } catch (err) {}
 };
 
 export const addNoteCategory = (values, callback) => async (dispatch) => {
-  try {
-    const userId = firebase.auth().currentUser.uid;
-    const response = await firebase
-      .firestore()
-      .collection('notesCategories')
-      .add({ name: values.name, todos: [], userId });
-
-    const foo = await (await response.get()).data();
-    const data = { ...foo, id: response.id };
-
-    dispatch(
-      {
-        type: ADD_NOTE_CATEGORY,
-        payload: data,
-      },
-      callback(),
-    );
-  } catch (err) {}
+  // try {
+  //   const userId = firebase.auth().currentUser.uid;
+  //   const response = await firebase
+  //     .firestore()
+  //     .collection('notesCategories')
+  //     .add({ name: values.name, todos: [], userId });
+  //   const foo = await (await response.get()).data();
+  //   const data = { ...foo, id: response.id };
+  //   dispatch(
+  //     {
+  //       type: ADD_NOTE_CATEGORY,
+  //       payload: data,
+  //     },
+  //     callback(),
+  //   );
+  // } catch (err) {}
 };
 
 export const deleteNoteList = (id) => async (dispatch) => {
-  try {
-    await firebase.firestore().collection('notesCategories').doc(id).delete();
-    dispatch({
-      type: DELETE_NOTE_LIST,
-      payload: id,
-    });
-  } catch (err) {}
+  // try {
+  //   await firebase.firestore().collection('notesCategories').doc(id).delete();
+  //   dispatch({
+  //     type: DELETE_NOTE_LIST,
+  //     payload: id,
+  //   });
+  // } catch (err) {}
 };
 
 export const sortNoteItems = () => {
---
+++ src/utils/api/index.js
@@ -0,0 +1,24 @@
+export const api = {
+  post: async (url, data = {}) => {
+    const token = localStorage.getItem('auth-token');
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify(data),
+    });
+  },
+  get: async (url, data = {}) => {
+    const token = localStorage.getItem('auth-token');
+    await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify(data),
+    });
+  },
+};
