8c9b44298f5741ae9bd2fa54f055c66f38d539db
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-02-28 23:49:01 +0100

    fix api helper errors

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,7 +1,9 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export const api = {
   post: async (url, data = {}) => {
     const token = localStorage.getItem('auth-token');
-    return await fetch(url, {
+    return await fetch(`${BASE_URL}${url}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -10,15 +12,14 @@ export const api = {
       body: JSON.stringify(data),
     });
   },
-  get: async (url, data = {}) => {
+  get: async (url) => {
     const token = localStorage.getItem('auth-token');
-    return await fetch(url, {
+    return await fetch(`${BASE_URL}${url}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: token,
       },
-      body: JSON.stringify(data),
     });
   },
 };
